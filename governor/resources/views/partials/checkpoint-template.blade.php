722634103222709a1ae80cfcf6f6804e829c0918
Andrew Earls <andrewearls@rocketmail.com>
2017-12-22 00:30:22 -0800

    added the checkpoint template

---
+++ governor/resources/views/partials/checkpoint-template.blade.php
@@ -0,0 +1,27 @@
+
+@extends('layouts.app')
+
+@section('checkpoint')
+	<div class="row no-gutters checkpoint">
+		<div class="col">
+			<div class="left-image">
+				<img src="http://lorempixel.com/500/500">
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="content">
+				<div class="title">
+					Title
+				</div>
+				<div class="description">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="right-image">
+				<img src="http://lorempixel.com/500/500">
+			</div>
+		</div>
+	</div>
+@endsection
